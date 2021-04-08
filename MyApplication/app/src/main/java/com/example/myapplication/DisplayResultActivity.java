f8736a2d502ffa945c825ccd2c0e7ab1a0d12efb
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2021-04-08 19:58:49 +0200

    Zadanie 2

---
+++ MyApplication/app/src/main/java/com/example/myapplication/DisplayResultActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_result);
+
+        Intent intent = getIntent();
+
+        String bmi = intent.getStringExtra(MainActivity.EXTRA_BMI);
+        String hb = intent.getStringExtra(MainActivity.EXTRA_HB);
+
+        TextView textView1 = findViewById(R.id.textView6);
+        textView1.setText(bmi);
+
+        TextView textView2 = findViewById(R.id.textView8);
+        textView2.setText(hb);
+
+        TextView textView3 = findViewById(R.id.textView10);
+
+        if(Double.valueOf(bmi) <= 18.49) {
+            textView3.setText("Egg-fried rice");
+        } else if(Double.valueOf(bmi) <= 24.99) {
+            textView3.setText("Pizza");
+        } else if(Double.valueOf(bmi) <= 29.99) {
+            textView3.setText("Summer Asian Slaw");
+        } else if(Double.valueOf(bmi) >= 25.0) {
+            textView3.setText("Best Broccoli Salad");
+        }
+
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_BMI = "com.example.myapplication.EXTRA_BMI";
+    public static final String EXTRA_HB = "com.example.myapplication.EXTRA_HB";
+    private String gender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+
+        Intent intent = new Intent(this, DisplayResultActivity.class);
+
+        EditText weightEditText = (EditText) findViewById(R.id.editTextNumberDecimal4);
+        Double weight = Double.valueOf(weightEditText.getText().toString());
+
+        EditText heightEditText = (EditText) findViewById(R.id.editTextNumberDecimal5);
+        Double height = Double.valueOf(heightEditText.getText().toString());
+
+        EditText ageEditText = (EditText) findViewById(R.id.editTextNumberDecimal6);
+        Double age = Double.valueOf(ageEditText.getText().toString());
+
+        Double bmi = weight/(height*height);
+
+        Double hb;
+
+        if(this.gender == "male")
+            hb = 66.47 + (13.7 * weight) + (5.0 * (height * 100)) - (6.76 * age);
+        else
+            hb = 655.1 + (9.567 * weight) + (1.85 * (height * 100)) - (4.68 * age);
+
+        intent.putExtra(EXTRA_BMI, String.valueOf(bmi));
+        intent.putExtra(EXTRA_HB, String.valueOf(hb));
+
+        startActivity(intent);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.checkBoxFemale:
+                if (checked)
+                    this.gender = "female";
+                break;
+            case R.id.checkBoxMale:
+                if (checked)
+                    this.gender = "male";
+                break;
+        }
+
+    }
+}
